b41d80915f5a60b4b6110b20b8d644a0de07ba57
Adedeji John Abidemi <adedejijohn099@gmail.com>
2025-10-03 19:25:04 -0500

    create the sidebar and add functionality to it

---
+++ index.js
@@ -0,0 +1,36 @@
+const showsidebar = document.querySelector(".fa-bars");
+const closebar = document.querySelector(".fa-xmark");
+const sidebar = document.querySelector(".sidebar");
+const navbar = document.querySelector("nav");
+const homeBg = document.querySelector("#home-bg");
+const header = document.querySelector("header");
+
+// showsidebar
+showsidebar.addEventListener("click", () => {
+  sidebar.style.display = "flex";
+});
+
+// closebar
+closebar.addEventListener("click", () => {
+  sidebar.style.display = "none";
+});
+
+// each list should close the sidebar when click and navigate
+document.querySelectorAll(".sidebar, li").forEach((link) => {
+  link.addEventListener("click", () => {
+    sidebar.style.display = "none";
+  });
+});
+
+// scrolling effect for the navbar
+window.addEventListener("scroll", () => {
+  const headerbottom = header.offsetHeight;
+  header;
+  if (window.scrollY > headerbottom) {
+    navbar.classList.add("scrolled");
+    homeBg.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+    homeBg.classList.remove("scrolled");
+  }
+});
